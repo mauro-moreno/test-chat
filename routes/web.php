3ccae0d025cf25a768eb02f77068d6fb1c71c9a3
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2019-06-30 03:13:10 -0300

    #5 Route regexp

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes(['register' => false]);
 
-Route::get('/{locale}', function ($locale) {
+Route::get('/{locale?}', function ($locale = 'en') {
     App::setLocale($locale);
     return view('welcome');
-});
+})->where('locale', '[a-z]{2}');
 
 
-Route::get('/test/{locale}', function ($locale) {
+Route::get('/test/{locale?}', function ($locale = 'en') {
     App::setLocale($locale);
     return view('test');
-});
+})->where('locale', '[a-z]{2}');
